e16a046f888e63af66550fc657bf979e22930c96
manyayadav <94862658+manyayadav@users.noreply.github.com>
2023-06-05 22:15:58 +0530

    Add files via upload

---
+++ TextToSpeechConverter.py
@@ -0,0 +1,19 @@
+# Import the Gtts module for text to speech conversion 
+from gtts import gTTS 
+  
+# import Os module to start the audio file
+import os 
+  
+mytext = "hello,how are you doing?"
+  
+# Language we want to use 
+language = 'en'
+  
+
+myobj = gTTS(text=mytext, lang=language, slow=False) 
+  
+
+myobj.save("output.mp3") 
+  
+# Play the converted file 
+os.system("start output.mp3") 
